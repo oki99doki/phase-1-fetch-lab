d630477272fbe5f317a958000be07b96f4112826
Stefan Klinkert <s.klinkert@gmail.com>
2024-06-30 22:49:46 -0400

    worked on lab, partially complete

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+  fetch("https://anapioficeandfire.com/api/books")
+  .then(resp => resp.json())
+  //.then((books) => console.log(books))
   
+  .then(books => renderBooks(books))
+  //return fetchBooks()
+
+  //.then((json) => console.log(renderBooks(json)));
+  //return fetchBooks(books)
 }
 
 function renderBooks(books) {
