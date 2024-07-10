1624a06ba42754561359da7b23d3e5bca84c105d
Stefan Klinkert <s.klinkert@gmail.com>
2024-07-10 14:30:31 -0400

    complete fetch lab - should be fine

--- index.js
+++ index.js
@@ -4,22 +4,26 @@ function fetchBooks() {
   .then(resp => resp.json())
   //.then((books) => console.log(books))
   
-  .then(books => renderBooks(books))
-  //return fetchBooks()
-
+  .then(json => renderBooks(json))
+  //.catch(error => console.log(error))
+  //return fetchBooks(book)
+  //return resp.json
+}
   //.then((json) => console.log(renderBooks(json)));
   //return fetchBooks(books)
-}
+
 
 function renderBooks(books) {
+  //console.log(books)
   const main = document.querySelector('main');
   books.forEach(book => {
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
     main.appendChild(h2);
+    //console.log(book)
   });
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
+  fetchBooks()
+})
